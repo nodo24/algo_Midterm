901dad5fa4fc14a07de7e2488a56ee2e8af6c8ef
nodo24 <nsana15@freeuni.edu.ge>
2018-12-01 20:14:14 +0400

    quick fix on  B.cpp

--- B.cpp
+++ B.cpp
@@ -6,11 +6,23 @@ using namespace std;
 int d[112345][30];
 int B[30];
 string a, b;
+int len_a, len_b;
+
+int check(int i) {
+    int err = 0;
+    for (int j = 0; j < 26; j++) {
+        int curr = d[i][j] - d[i - len_b][j];
+        if (curr != B[j]) {
+            err = 1;
+        }
+    }
+    return err;
+}
 
 int main() {
     cin >> a >> b;
-    int len_b = b.length();
-    int len_a = a.length();
+    len_b = b.length();
+    len_a = a.length();
     if (len_a < len_b) {
         cout << "NO";
         return 0;
@@ -20,24 +32,21 @@ int main() {
     }
 
     for (int i = 0; i < len_a; i++) {
-        d[i+1][a[i] - 'a']++;
+        d[i + 1][a[i] - 'a']++;
         for (int j = 0; j < 26; j++) {
-            d[i + 2][j] = d[i+1][j];
+            d[i + 2][j] = d[i + 1][j];
         }
     }
-
-    for (int i = len_b; i <= len_a; i++) {
-        int err = 0;
-        for (int j = 0; j < 26; j++) {
-            int curr = d[i][j] - d[i - len_b][j];
-            if (curr != B[j]) {
-                err = 1;
-            }
+    int index = len_b;
+    while (true) {
+        if (index > len_a) {
+            break;
         }
-        if (err == 0) {
+        if (check(index) == 0) {
             cout << "YES";
             return 0;
         }
+        ++index;
     }
     cout << "NO";
     return 0;
